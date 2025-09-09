9ec7d7c107b724aaad164244839808f0d33b2586
Daniel Fuentealba <danielfuentealba86@gmail.com>
2025-09-09 21:00:51 +0200

    token visible in localStorage again

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -8,15 +8,13 @@ const signToken = (userId) =>
 export const createSendToken = function (user, statusCode, res) {
   const token = signToken(user.id);
 
-  const isProd = process.env.NODE_ENV === "production";
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: isProd,
-    sameSite: "lax",
-    path: "/",
-    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-  });
-
+  // Remove password from user object before sending
   user.password = undefined;
-  return res.status(statusCode).json({ status: "success", token, user });
+
+  // Return token in response body for localStorage storage
+  return res.status(statusCode).json({
+    status: "success",
+    token,
+    user,
+  });
 };
